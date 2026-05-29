2aff3d40fe75c0686735d4eb9f58165d3e743289
AVDHESH KUMAR DADHICH <itzzavdhesh@gmail.com>
2026-05-29 18:21:17 +0530

    Update crypto.js

--- client/src/utils/crypto.js
+++ client/src/utils/crypto.js
@@ -1,15 +1,1 @@
-export function encryptStore(value) {
-  try {
-    return btoa(encodeURIComponent(value));
-  } catch {
-    return value;
-  }
-}
 
-export function decryptStore(value) {
-  try {
-    return decodeURIComponent(atob(value));
-  } catch {
-    return value;
-  }
-}
